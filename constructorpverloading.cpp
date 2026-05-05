86bcf9e9909e608341a0e2a7ed9e6873acbac6dc
kalycaokta-ux <kalycaokta@gmail.com>
2026-05-05 20:12:25 +0700

    menambahkan mhs1.cetak

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -14,9 +14,36 @@ class mahasiswa{
     void cetak();
 };
 
-mahasiswa::mahasiswa(){
+mahasiswa :: mahasiswa(){
 }
 
 mahasiswa::mahasiswa(int iNim){
     nim = iNim;
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama;
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+
+    cout << "NIM    : " << nim << endl;
+    cout << "Nama   : " << nama << endl;
+}
+
+int main() {
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(010203, "Santi");
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
 }
