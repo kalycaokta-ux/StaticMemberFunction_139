247f716339192275ac95a4b2b3f54c52d42c5d9c
kalycaokta-ux <kalycaokta@gmail.com>
2026-05-05 19:50:53 +0700

    menambahkan variabel judul

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -2,4 +2,21 @@
 using namespace std;
 class buku{
     string judul;
-    
+
+    public:
+        buku &setJudul(string judul) {
+            this->judul = judul;
+            return *this; // chain function
+        }
+
+        string getJudul() {
+            return this->judul;
+             }
+};
+
+int main() 
+{
+    buku bukunya;
+    cout << bukunya.setJudul("Matematika").getJudul(); 
+    return 0;
+}
